f2a3e7c4b057977f731158f749cdebbb3b43d31a
jfollert <josefollertm@gmail.com>
2023-12-27 16:44:37 -0300

    refactor: add 'type' field to Category model with 'expense' or 'income' choices for data validation

--- categories/models.py
+++ categories/models.py
@@ -3,9 +3,17 @@ from django.utils import timezone
 import uuid
 
 class Category(models.Model):
+  EXPENSE = 'expense'
+  INCOME = 'income'
+    
+  TYPE_CHOICES = [
+    (EXPENSE, 'Expense'),
+    (INCOME, 'Income'),
+  ]
+  
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
   name = models.CharField(max_length=255)
-  type = models.CharField(max_length=50)
+  type = models.CharField(max_length=50, choices=TYPE_CHOICES)
   created_at = models.DateTimeField(default=timezone.now)
   updated_at = models.DateTimeField(default=timezone.now)
 
